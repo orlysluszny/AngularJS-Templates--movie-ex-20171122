765fa63ea3cc94518d45b2e14c9ab503c62bf6a6
Orly Sluszny <orly.sluszny@gmail.com>
2017-11-22 13:59:49 +0200

    time final

--- convertService.js
+++ convertService.js
@@ -1,59 +1,10 @@
-app.factory("convert", function() {
-    
-      /*var timeUnits = ["hr", "min"];
-      var time = {
-        hr: 1,
-        min: 60
-      };
-    
-      function convertTime(time, inUnit, outUnit) {
-        return Math.round(distance * kmRates[outUnit] / kmRates[inUnit]);
-      }
-    
-      function convertKMToMile (km) {
-        return Math.round(km * 0.621371192);
-      }
-      
-      return {
-        distance: convertDistance
-      }
-    });*/
-
+app.factory("convert", function() {    
     var timeConvert = function(n){
         var min = n%60
         var hr = (n - min) / 60
          return hr + ":" + min;
        }
-
-
        return {
         time: timeConvert
       }
-
-    
     });
-/*
-    app.factory("convert", function() {
-        
-          var distanceUnits = ["KM", "METER", "MILE", "FEET"];
-          var kmRates = {
-            KM: 1,
-            METER: 1000,
-            MILE: 0.621371192,
-            FEET: 3280.8399
-          };
-        
-          function convertDistance(distance, inUnit, outUnit) {
-            return Math.round(distance * kmRates[outUnit] / kmRates[inUnit]);
-          }
-        
-          function convertKMToMile (km) {
-            return Math.round(km * 0.621371192);
-          }
-          
-          return {
-            /*km2Mile: convertKMToMile*/
-            /*distance: convertDistance
-          }
-        });
-        */
--- movieCtrl.js
+++ movieCtrl.js
@@ -1,8 +1,8 @@
-app.controller("movieCtrl", function ($scope, $http, convert) {
+app.controller("movieCtrl", function ($scope, $http, convert, $log) {
 
         function Movie(name, time, director,actors, link) {
             this.name = name;
-            this.time = time;
+            this.time = convert.time(time);
             this.director = director;
             this.actors = actors;
             this.link = link;
@@ -10,9 +10,13 @@ app.controller("movieCtrl", function ($scope, $http, convert) {
     
         $scope.movies = [];
         $http.get("movie.json").then(function mySuccess(response) {
-            $scope.movies=response.data;
+                for (var i = 0; i < response.data.length; i++) {
+                  $scope.movies.push(new Movie(response.data[i].name, response.data[i].time, 
+                    response.data[i].director, response.data[i].actors, response.data[i].link))  
+                }
+            //$scope.movies=response.data;
         }, function myError(response) {
             alert("error" + JSON.stringify(response.status));
           })
-          $log.log(convert.time(n));
+         /* $log.log(convert.time(n));*/
         });
